5b9dd59c053155fec9dc9810359208f3d56ab01d
Bazuev Ivan <64332115+evanesenze@users.noreply.github.com>
2023-04-03 17:08:39 +0500

    env fix

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,12 @@ import { AuthModule } from './modules/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MongooseModule.forRoot('mongodb+srv://evanesenze:ZXCpoi12@base.6n0ujy2.mongodb.net/?retryWrites=true&w=majority'), AuthModule, AppModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.BASE_URL),
+    AuthModule,
+    AppModule,
+    UsersModule
+  ],
 })
 export class AppModule { }
